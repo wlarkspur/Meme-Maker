e75dd7297f392b4aaaeb3597632020f9db436126
Seokmin <newacon@naver.com>
2022-10-09 22:43:13 +0200

    color change of painter

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
+const color = document.getElementById("color");
+const LineWidth = document.getElementById("Line-width");
 const canvas = document.querySelector("canvas");
 
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = LineWidth;
+let isPainting = false;
 
-ctx.fillRect(50, 50, 100, 200);
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+canvas.addEventListener("mousemove", onMove);
+document.addEventListener("mousedown", startPainting);
+document.addEventListener("mouseup", cancelPainting);
+document.addEventListener("mouseleave", cancelPainting);
+
+LineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOption.forEach((color) => color.addEventListener("click", onColorClick));
