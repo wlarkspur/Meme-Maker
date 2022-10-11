2568697fb63c293c8278be886e73956e24ca8c66
Seokmin <newacon@naver.com>
2022-10-11 22:44:03 +0200

    Text Input

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const textInput = document.getElementById("text");
+const file = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraseBtn = document.getElementById("eraser-btn");
@@ -14,6 +16,7 @@ const CANVSD_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVSD_HEIGHT;
 ctx.lineWidth = LineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -66,23 +69,47 @@ function onModeClick() {
 function onCanvasClick() {
   if (isFilling) {
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVSD_HEIGHT);
-    beginPath();
   }
 }
 
 function destroyButton() {
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVSD_HEIGHT);
-  beginPath();
 }
 
 function eraseButton() {
   ctx.strokeStyle = "white";
   isFilling = false;
   modeBtn.innerText = "Fill";
-  beginPath();
 }
 
+/**function dot(event) {
+  ctx.strokeRect(event.offsetX, event.offsetY, 1, 1);
+}**/
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVSD_HEIGHT);
+    file.value = null;
+  };
+}
+
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "50px serit";
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 document.addEventListener("mousedown", startPainting);
 document.addEventListener("mouseup", cancelPainting);
@@ -97,3 +124,5 @@ colorOption.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", destroyButton);
 eraseBtn.addEventListener("click", eraseButton);
+//canvas.addEventListener("click", dot);
+file.addEventListener("change", onFileChange);
